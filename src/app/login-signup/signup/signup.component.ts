81c465280d6a1680dfd0cdb29c14fad84979d931
Vanz1233 <B2300277@helplive.edu.my>
2025-02-28 02:26:32 +0800

    Your commit message

--- src/app/login-signup/signup/signup.component.ts
+++ src/app/login-signup/signup/signup.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-signup',
@@ -6,5 +8,35 @@ import { Component } from '@angular/core';
   styleUrl: './signup.component.css'
 })
 export class SignupComponent {
+  signupForm: FormGroup;
+
+  constructor(private fb: FormBuilder, private http: HttpClient) {
+    this.signupForm = this.fb.group({
+      fullName: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      phone: ['', [Validators.required, Validators.pattern('^\\+?[0-9]{10,15}$')]],
+      username: ['', Validators.required],
+      password: ['', [Validators.required, Validators.minLength(6)]]
+    });
+  }
+
+  onSubmit() {
+    if (this.signupForm.valid) {
+      const formData = this.signupForm.value;
+      this.http.post('http://localhost:3000/api/signup', formData).subscribe(
+        response => {
+          console.log('User registered successfully:', response);
+          alert('Signup successful!');
+        },
+        error => {
+          console.error('Signup failed:', error);
+          alert('Signup failed. Please try again.');
+        }
+      );
+    } else {
+      alert('Please fill in all required fields correctly.');
+    }
+  }
+  
 
 }
